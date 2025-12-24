ac8833ee9a24bcce196fd1b50c895a669f81e59e
Rajesh Ramachandran <RajeshR0204@gmail.com>
2025-12-24 20:47:08 +0530

    Working code and tested

---
+++ BodyMap/bodymap-server/contracts/bodymap.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.19;
+
+contract BodyMap
+{
+    bytes32 private passwordHash;
+    string public basicBodyMap;
+    string public tailorBodyMap;
+
+    constructor(string memory passwordClearText)
+    {
+        passwordHash = keccak256(abi.encodePacked(passwordClearText));
+    }
+
+    function setBodyMaps(string memory passwordClearText, string memory newBasicBodyMap, string memory newTailorBodyMap) public 
+    {
+        bytes32 givenPasswordHash = keccak256(abi.encodePacked(passwordClearText));
+        if(passwordHash == givenPasswordHash)
+        {
+            basicBodyMap = newBasicBodyMap;
+            tailorBodyMap = newTailorBodyMap;
+        }
+    }
+}
